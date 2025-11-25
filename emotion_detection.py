b3f2f1f8b025b93194ef91655906564f7f25cef1
Greg Bui <greg.bui@gmail.com>
2025-11-24 19:19:10 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,14 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
 
     resp = requests.post(url, json=input_json, headers=headers)
-    return resp.text
+    f_resp = json.loads(resp.text)
+
+    anger_score = f_resp['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = f_resp['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = f_resp['emotionPredictions'][0]['emotion']['fear']
+    joy_score = f_resp['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = f_resp['emotionPredictions'][0]['emotion']['sadness']
+    dominant = 'joy'
+
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, \
+    'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant}
