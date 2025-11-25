63720a3631230890c744bc220070a9efa4a46ae4
Greg Bui <greg.bui@gmail.com>
2025-11-24 20:34:20 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,12 +9,28 @@ def emotion_detector(text_to_analyse):
     resp = requests.post(url, json=input_json, headers=headers)
     f_resp = json.loads(resp.text)
 
-    anger_score = f_resp['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = f_resp['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = f_resp['emotionPredictions'][0]['emotion']['fear']
-    joy_score = f_resp['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = f_resp['emotionPredictions'][0]['emotion']['sadness']
-    dominant = 'joy'
+    anger_score = float(f_resp['emotionPredictions'][0]['emotion']['anger'])
+    dominant = 'anger'
+    dominant_score = anger_score
+
+    disgust_score = float(f_resp['emotionPredictions'][0]['emotion']['disgust'])
+    if disgust_score > dominant_score:
+        dominant = 'disgust'
+        dominant_score = disgust_score
+
+    fear_score = float(f_resp['emotionPredictions'][0]['emotion']['fear'])
+    if fear_score > dominant_score:
+        dominant = 'fear'
+        dominant_score = fear_score
+
+    joy_score = float(f_resp['emotionPredictions'][0]['emotion']['joy'])
+    if joy_score > dominant_score:
+        dominant = 'joy'
+        dominant_score = joy_score
+
+    sadness_score = float(f_resp['emotionPredictions'][0]['emotion']['sadness'])
+    if sadness_score > dominant_score:
+        dominant = sadness
 
     return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, \
             'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant}
